f07be307613fb012f94fc8275136c9c3578f45ca
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-21 14:08:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index do |dwarve, index|
-    puts "#{index}. #{dwarve}"
+    puts "#{index + 1}. #{dwarve}"
   end
   
 end
